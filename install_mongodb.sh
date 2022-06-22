7bad4e16b0c2a2e4bc6362d5b7cebc6cb8dfb89d
Sergey Klimenko <serjastral@gmail.com>
2022-06-22 10:55:40 +0300

    Add install mongodb-org in install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 sudo apt update
+
+### Not work in Russian:
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
+###
+
 sudo apt install -y mongodb
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
